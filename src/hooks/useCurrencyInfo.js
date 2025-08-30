3b5e213026864f89b3a1e67d99f09109c805e77c
muhammadkhan-dev-code <muhammadkhansw00@gmail.com>
2025-08-30 10:30:42 +0500

    Currency converter

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    if (!currency) return;
+
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((res) => {
+        setData(res[currency]);
+      })
+      .catch((err) => {
+        console.error("Error fetching currency data:", err);
+      });
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
